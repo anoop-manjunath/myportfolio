ccbd956000f7a580e1426128bbec4118e3cfc35f
anoopdm <anoopdm11@gmail.com>
2026-05-05 16:19:02 +0530

    Fix CI lint warnings for Vercel deploy

--- src/Components/About.js
+++ src/Components/About.js
@@ -16,7 +16,7 @@ const About = () => {
             I have done an internship as a <b>software developer</b> at GoalFi which is a fintech startup.
             I love to create original projects with beautiful designs, you can check out some of my work in the projects section.<br/><br/>
             I am <b>open</b> to new collaborations or work where I can contribute and grow. Feel free to connect with me, links are in the footer.<br/>
-            Apart from coding I love to do photography, you can check out some of my shots here <a href="https://www.instagram.com/devansh_sahni_/?hl=en" target='_blank'>Instagram.</a>
+            Apart from coding I love to do photography, you can check out some of my shots here <a href="https://www.instagram.com/devansh_sahni_/?hl=en" target='_blank' rel='noreferrer'>Instagram.</a>
           </p>
         </div>
 
